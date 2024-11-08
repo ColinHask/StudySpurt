adf68b49a873bb4e2ae0e26bdc468bd5bd832bd7
ColinHask <colinhaskins2022@gmail.com>
2024-11-08 00:32:38 -0500

    pyqt5 testing/learning

---
+++ pyqt5 tesing/sample_pyqt5.py
@@ -0,0 +1,28 @@
+import sys
+
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        # you must always call the super __init__ function
+        # to allow Qt to set up the object.
+        super().__init__()
+
+        self.setWindowTitle("Bingus Bongus")
+        button = QPushButton("Press Me!")
+
+        self.setFixedSize(QSize(400, 300))
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
